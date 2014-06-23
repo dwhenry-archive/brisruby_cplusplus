6eabebabc9cb0e71633c2c322164e3641b7398ba
Jamie Cook <jamie.cook@veitchlister.com.au>
2014-06-23 15:07:04 +1000

    remove line

--- extension_in_5_min/MyTest.c
+++ extension_in_5_min/MyTest.c
@@ -14,7 +14,6 @@ VALUE method_sum(VALUE self, VALUE x, VALUE y);
 void Init_mytest() {
 	MyTest = rb_define_module("MyTest");
   rb_define_singleton_method(MyTest, "sum", method_sum, 2);
-	//rb_define_method(MyTest, "sum",   method_sum,   2);
 }
 
 VALUE method_sum(VALUE self, VALUE x, VALUE y) {
