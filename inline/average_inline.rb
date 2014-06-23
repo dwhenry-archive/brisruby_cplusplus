d36081c756d0034ef1c7f754e5ec6fc0e711d537
Jamie Cook <jamie.cook@veitchlister.com.au>
2014-06-23 14:47:48 +1000

    simplify

--- inline/average_inline.rb
+++ inline/average_inline.rb
@@ -1,10 +1,17 @@
 require 'inline'
+require 'benchmark'
 
 class Array
 
+  def average_ruby
+    result = 0
+    self.each { |x| result += x }
+    result / self.size.to_f
+  end
+
   inline do |builder|
     builder.c_raw "
-      static VALUE average(int argc, VALUE *argv, VALUE self) {
+      static VALUE average_c(int argc, VALUE *argv, VALUE self) {
         double result = 0;
         long  i, len;
         VALUE *arr = RARRAY_PTR(self);
@@ -20,12 +27,11 @@ class Array
   end
 end
 
-max_loop = (ARGV.shift || 5).to_i
+max_loop = (ARGV.shift || 100).to_i
 max_size = (ARGV.shift || 100_000).to_i
 a = (1..max_size).to_a
 
-1.upto(max_loop) do
-  avg = a.average
-  $stderr.print "."
-end
-$stderr.puts ""
+Benchmark.bm { |x| 
+  x.report(" pure ruby: ") { 1.upto(max_loop) { avg = a.average_ruby } }
+  x.report("inline c++: ") { 1.upto(max_loop) { avg = a.average_c } }
+}
--- inline/do_bench.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-time ruby average_pure.rb   500
-time ruby average_inline.rb 500
